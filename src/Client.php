f52deee39828c8ce742b1e91fcb0eb9b45661cdc
pxgamer <owzie123@gmail.com>
2018-03-12 22:36:16 +0000

    Add Client class with base URI

---
+++ src/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace pxgamer\Gitcoin;
+
+/**
+ * Class Client
+ */
+class Client
+{
+    /**
+     * The base URI to connect with.
+     */
+    public const BASE_URI = 'https://gitcoin.co/api/v0.1';
+}
