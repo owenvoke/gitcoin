37345a15a74c965bbf1ef9de65c148ca45084f91
pxgamer <owzie123@gmail.com>
2018-03-13 09:49:21 +0000

    Add GuzzleHttp\Client property to Client

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,8 @@
 
 namespace pxgamer\Gitcoin;
 
+use GuzzleHttp\Client as GuzzleClient;
+
 /**
  * Class Client
  */
@@ -10,5 +12,19 @@ class Client
     /**
      * The base URI to connect with.
      */
-    public const BASE_URI = 'https://gitcoin.co/api/v0.1';
+    public const BASE_URI = 'https://gitcoin.co/api/v0.1/';
+    /**
+     * @var GuzzleClient
+     */
+    private $client;
+
+    /**
+     * Basic constructor.
+     */
+    public function __construct()
+    {
+        $this->client = new GuzzleClient([
+            'base_uri' => self::BASE_URI,
+        ]);
+    }
 }
